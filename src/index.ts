6e3e4edcb894ea51d2859f739fe9a18d9a303f8b
TheChiran <tonmoychiran449@gmail.com>
2024-10-22 22:05:49 +0600

    Added image tag as env

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ const app = express();
 const PORT = 8000;
 
 app.get("/", async function (req, res) {
-  res.send(`Greeting from Chiran, Tonmoy, DateTime: ${new Date()} `);
+  res.send(
+    `Greeting from Chiran, Tonmoy, Changed docker-compose image with dynamic env values. DateTime: ${new Date()} `
+  );
 });
 
 app.listen(PORT, async function () {
